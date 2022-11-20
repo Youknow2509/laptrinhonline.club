c9e33af810fea32007160a8a3b6c72ec49bd0289
V-Mac <lytranvinh.2509@gmail.com>
2022-11-20 21:54:11 +0700

    20/11/22 push mac_v

--- code/hhb3.cpp
+++ code/hhb3.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int main(){
     
-    
+    int a;
+    cin >> a;
+    if ( a % 3 == 0) cout << "YES";
+    else cout << "NO";
 
     return 0;   
 }
---
+++ code/i9511.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int a;
+    while(a != 0){
+        
+    }
+
+    return 0;
+}
